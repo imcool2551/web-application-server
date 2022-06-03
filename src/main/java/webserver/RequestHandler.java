5c46b46fd7f0ffc868a745442ab8214881d16490
imcool2551 <imcool2551@gmail.com>
2022-06-03 15:53:53 +0900

    feat: POST signup

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,12 +3,16 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
+import util.IOUtils;
+
+import static util.HttpRequestUtils.*;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -27,14 +31,20 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String firstLine = br.readLine();
-            logHeaders(br);
-
             String requestMethod = firstLine.split(" ")[0];
             String requestURL = firstLine.split(" ")[1];
 
-            if (requestMethod.equals("GET") && requestURL.startsWith("/user/create")) {
-                String params = requestURL.substring(requestURL.indexOf("?") + 1);
-                Map<String, String> queryString = HttpRequestUtils.parseQueryString(params);
+            List<Pair> headers = parseHeaders(br);
+
+            if (requestMethod.equals("POST") && requestURL.startsWith("/user/create")) {
+                int contentLength = headers.stream()
+                        .filter(header -> header.getKey().equals("Content-Length"))
+                        .map(header -> Integer.parseInt(header.getValue()))
+                        .findAny()
+                        .orElse(0);
+
+                String bodyData = IOUtils.readData(br, contentLength);
+                Map<String, String> queryString = parseQueryString(bodyData);
                 User user = new User(queryString.get("userId"),
                         queryString.get("password"),
                         queryString.get("name"),
@@ -62,12 +72,17 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private void logHeaders(BufferedReader br) throws IOException {
+    private List<Pair> parseHeaders(BufferedReader br) throws IOException {
+        List<Pair> headers = new ArrayList<>();
+
         String headerLine = br.readLine();
         while (headerLine != null && !headerLine.equals("")) {
             log.info(headerLine);
+            headers.add(parseHeader(headerLine));
             headerLine = br.readLine();
         }
+
+        return headers;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
